b150ac2ed2624d26c6a4d55521d9cb76cfeecdcf
Shtymak Maryan <sht.marian@gmail.com>
2020-03-20 17:49:58 +0200

    fix tree

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,5 @@
-import { VideoCapture } from './VideoCapture-old'
-import CanvasCapture from './CanvasCapture/CanvasCapture'
+import { Stream } from './lib/stream'
 
-// import * as poseNet from '@tensorflow-models/posenet';
+const stream = new Stream()
 
-let canvasCapture = null
-let videoCapture = new VideoCapture({
-  audio: false,
-  hide: true,
-  frameRate: 10,
-  onCreated: (video: HTMLVideoElement) => {
-    canvasCapture = new CanvasCapture({ 
-      video,
-      rotate: 0,
-      onCreated: () => {
-        
-      },
-      onFrameUpdated: () => {
-
-      }
-     })
-    canvasCapture.createCanvase()
-    canvasCapture.start()
-  }
-})
-videoCapture.createVideo()
-videoCapture.play()
+console.log(stream);
