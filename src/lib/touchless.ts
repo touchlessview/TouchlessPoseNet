bd715447a28b8b08ef770b1e761ba3eb4e5d759c
Shtymak Maryan <sht.marian@gmail.com>
2020-04-04 09:37:47 +0300

    cleaning

--- src/lib/touchless.ts
+++
@@ -1,9 +0,0 @@
-import { Observable, Subject } from 'rxjs';
-import { TouchlessEvent } from './touchless.types';
-
-export class Touchless {
-  get events$(): Observable<TouchlessEvent> { return this._events$.asObservable(); }
-  private _events$: Subject<TouchlessEvent>;
-
-  constructor() {}
-}
